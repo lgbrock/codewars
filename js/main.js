259bd7eb75fdcd0741d0f267b2f29e00ba9f29c4
Logan Brock <36110596+lgbrock@users.noreply.github.com>
2021-01-26 11:00:29 -0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function positiveSum(arr) {
+    let sum = 0
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            sum += arr[i]
+        }
+    }
+    return (sum)
+}
+let array = [1, 2, 3, 4]
+positiveSum(1, 2, 3, 4, 5)
+
+
