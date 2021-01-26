8186d79ea393d29972948467d8a5eb5d1451d02f
Logan Brock <36110596+lgbrock@users.noreply.github.com>
2021-01-26 11:24:11 -0600

    Got live server working

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
-function positiveSum(arr) {
-    let sum = 0
+// *Variables*
+// Declare a variable, reassign it to your fav holiday, make sure it is in all caps, and print the value to the console
 
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > 0) {
-            sum += arr[i]
-        }
-    }
-    return (sum)
-}
-let array = [1, 2, 3, 4]
-positiveSum(1, 2, 3, 4, 5)
+let favHoliday = 'christmas'
+favHoliday = favHoliday.toUpperCase()
+console.log(favHoliday)
 
+//Declare a variable, assign it a string, alert the last three characters in the string (Use your google-fu and the MDN)
 
+let lastThree = 'last3'
+alert(lastThree.slice(-3))
