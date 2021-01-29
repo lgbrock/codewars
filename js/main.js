e80b74b8a1070a1768adeeaa5faaab1bcb3f0ea3
Logan Brock <logan.g.brock@gmail.com>
2021-01-29 15:56:58 -0600

    update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-//---Easy
-//create a function that adds two numbers and alerts the sum
+function getCount(str) {
+    str = str.toLowerCase
+    let vowels = ('aeiou')
+    let vowelsCount = 0;
+    // enter your majic here
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.indexOf(str[i]) !== -1) {
+            vowelsCount++;
+        }
+    }
+    return vowelsCount;
+}
 
-
-  //create a function that divides two numbers and console logs the quotient
-
-  //create a function that multiplys four numbers and returns the product
-
-  //---Medium
-  //create a function that takes in four numbers. Add the first two numbers and return the remainder of dividing the sum of the first two numbers by the difference of the last two numbers
-
-
-  //---Hard
-  //create a function that takes in 4 numbers. Multiply the first two numbers. If the product is greater than 100 add the sum of the last two numbers and console log the value. If the product is less that 100, subtract the difference of the last two numbers and console log the value. If the product is 100, multiply the first three numbers together and alert the remainder of dividing the fourth number
+let solution = getCount('hi');
+console.log(solution)
