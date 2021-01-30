b6dfc3733523ae5d6f4176dd76e03abeb5148d3d
Logan Brock <logan.g.brock@gmail.com>
2021-01-30 10:19:20 -0600

    codeWars-vowelCount finished

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 function getCount(str) {
-    str = str.toLowerCase
-    let vowels = ('aeiou')
+    // str = str.toLowerCase
     let vowelsCount = 0;
+    const vowels = ['a', 'e', 'i', 'o', 'u']
     // enter your majic here
-    for (let i = 0; i < str.length; i++) {
-        if (vowels.indexOf(str[i]) !== -1) {
-            vowelsCount++;
+    for (let char of str) {
+        if (vowels.includes(char)) {
+            vowelsCount++
         }
     }
     return vowelsCount;
 }
 
-let solution = getCount('hi');
+let solution = getCount('abracadabra');
 console.log(solution)
