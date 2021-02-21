aa2bd54327a3af4849d9f01bd8561d42e8fd3c31
Logan Brock <logan.g.brock@gmail.com>
2021-02-21 15:47:41 -0600

    started adding titles to completed codewars

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Count the Monkeys 2.17.20
 function monkeyCount(n) {
   const monkeys = []
   for(i = 1; i<n+1; i++){
@@ -7,5 +8,7 @@ function monkeyCount(n) {
   }
 
 
-  
-// next question
+// Remove String Spaces 2.19.20
+function noSpace(x){
+  return x.split(" ").join('')
+  }
