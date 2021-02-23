534e4f114b2f02b3a4573ce6f9a3cb284f59b213
Logan Brock <logan.g.brock@gmail.com>
2021-02-22 20:01:13 -0600

    added more codewar challenges

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//Count the Monkeys 2.17.20
+// Count the Monkeys 2.17.20
 function monkeyCount(n) {
   const monkeys = []
   for(i = 1; i<n+1; i++){
@@ -12,3 +12,21 @@ function monkeyCount(n) {
 function noSpace(x){
   return x.split(" ").join('')
   }
+
+
+  // Beginner - Lost Without a Map
+  function maps(x){
+    const y = x.map(value => value *2)
+    return y
+  }
+  
+  maps([1,2,3])
+
+
+  // Find the smallest integer in the array
+  class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return args.sort((a,b) => a-b)[0]
+    }
+  }
+  
