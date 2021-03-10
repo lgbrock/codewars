63bd4876899a5dc12fd55cd09c85d5f16ef77e38
Logan Brock <logan.g.brock@gmail.com>
2021-03-10 11:01:00 -0600

    Calculate BMI codewar challenge 3.10.21

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
+// Calculate BMI
+function bmi(weight, height) {
+  let bmi = weight / Math.pow(height, 2);
+  
+  if(bmi <= 18.5){
+    return 'Underweight'
+  } else if (bmi <= 25.0) {
+    return 'Normal'
+  } else if (bmi <= 30.0) {
+    return 'Overweight'
+  } else {
+    return 'Obese'
+  }
+}
+
+bmi(180, 72)
+
+/*
 // L1: Set alarm
-function setAlarm(employed, vacation){
+unction setAlarm(employed, vacation){
   if(employed && !vacation){
     return true
   } else {
@@ -10,7 +28,6 @@ function setAlarm(employed, vacation){
 
 
 
-/*
 // Transportation on vacation
 //for(let i = 0; i < d.length; i++){ if d.length > 7}
 function rentalCarCost(d) {
