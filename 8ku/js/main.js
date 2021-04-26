022a9d0e045600bf324ccbbdb562e246aa26e7c8
Logan Brock <logan.g.brock@gmail.com>
2021-04-26 15:38:01 -0400

    students final grade codewars challenge

--- 8ku/js/main.js
+++ 8ku/js/main.js
@@ -1,5 +1,10 @@
-//
-
+// Student's Final Grade
+const finalGrade = (exam, projects) => {
+  if(exam > 90 || projects > 10) return 100;
+  if(exam > 75 & projects >= 5) return 90;
+  if(exam > 50 & projects >= 2) return 75;
+  return 0;
+}
 
 
 
