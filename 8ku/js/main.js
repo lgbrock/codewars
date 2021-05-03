4d66ac118c16658595714aab75874922c5e9ed53
Logan Brock <logan.g.brock@gmail.com>
2021-05-03 09:30:21 -0400

    Grasshopper - Bug Squashing codewars challenge

--- 8ku/js/main.js
+++ 8ku/js/main.js
@@ -1,4 +1,16 @@
-// next Kata
+// Grasshopper - Bug Squashing
+let health = 100
+let position = 0
+let coins = 0
+
+function main () {
+  rollDice()
+  move()
+  combat()
+  getCoins()
+  buyHealth()
+  printStatus()
+}
 
 
 
